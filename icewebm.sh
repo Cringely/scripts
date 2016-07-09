9a66e36c6dd837fc556b6ad83a0050f33e49f78c
nwgat <github@nwgat.net>
2016-07-09 22:32:17 +0200

    Update icewebm.sh

--- icewebm.sh
+++ icewebm.sh
@@ -4,6 +4,6 @@
 ffmpeg -i bbb_sunflower_1080p_30fps_normal.mp4 \
   -f webm -cluster_size_limit 2M -cluster_time_limit 5100 -content_type video/webm \
   -vf scale=1280:-1 -r 30 -ac 2 \
-  -acodec libopus -b:a 64K \
+  -acodec libopus -b:a 96K \
   -vcodec libvpx -b:v 2.5M -crf 30 -g 150 -deadline realtime -threads 8 \
   icecast://source:passwordP@ninja.domain:8000/live.webm
